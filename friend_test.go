b43dea7ea02d59d3acc32493a25031bf63032304
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2022-09-15 22:21:18 +0200

    test add and remove friend

--- friend_test.go
+++ friend_test.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"bytes"
+	"path"
 	"testing"
 )
 
-func TestFriend(t *testing.T) {
+func TestAddFriend(t *testing.T) {
 	dir := t.TempDir()
 	account, _ := NewAccount(dir, "Ahmed Mohamed", "ahmed@example.com", "strong password")
 
 	friend_dir := t.TempDir()
 	friend_account, _ := NewAccount(friend_dir, "Mohamed Mahmoud", "mohamed@example.com", "strong password")
+	fingerprint := friend_account.Fingerprint()
 	friend_account_pub, _ := friend_account.Export()
-	friend, _ := AddFriend(account, bytes.NewBuffer(friend_account_pub))
+	friend, err := AddFriend(account, bytes.NewBuffer(friend_account_pub))
+	ASSERT_ERROR(t, nil, err)
+	ASSERT_FILE_EXISTS(t, path.Join(dir, ".mau", fingerprint+".pgp"))
 
 	t.Run("Email", func(t T) {
 		ASSERT_EQUAL(t, "mohamed@example.com", friend.Email())
@@ -32,4 +36,19 @@ func TestFriend(t *testing.T) {
 		ASSERT_ERROR(t, nil, err)
 		ASSERT_EQUAL(t, friend_account_identity, friend_identity)
 	})
+}
+
+func TestRemoveFriend(t *testing.T) {
+	dir := t.TempDir()
+	account, _ := NewAccount(dir, "Ahmed Mohamed", "ahmed@example.com", "strong password")
+
+	friend_dir := t.TempDir()
+	friend_account, _ := NewAccount(friend_dir, "Mohamed Mahmoud", "mohamed@example.com", "strong password")
+	friend_account_pub, _ := friend_account.Export()
+	fingerprint := friend_account.Fingerprint()
+	friend, _ := AddFriend(account, bytes.NewBuffer(friend_account_pub))
+
+	err := RemoveFriend(account, friend)
+	ASSERT_ERROR(t, nil, err)
+	REFUTE_FILE_EXISTS(t, path.Join(dir, ".mau", fingerprint+".pgp"))
 }
